5c179ab5e261ffd4b555d1250c0918145041b738
Pedro Gomes <pedrogomes94@gmail.com>
2021-04-14 20:14:38 +0200

    update test

--- packages/provider/test/index.test.ts
+++ packages/provider/test/index.test.ts
@@ -14,8 +14,8 @@ describe("@json-rpc-tools/provider", () => {
   });
 
   it("WS", async () => {
-    const provider = new JsonRpcProvider(`wss://bridge.walletconnect.org`);
-    const request = formatJsonRpcRequest("bridge_subscribe", {
+    const provider = new JsonRpcProvider(`wss://staging.walletconnect.org`);
+    const request = formatJsonRpcRequest("waku_subscribe", {
       topic: "ca838d59a3a3fe3824dab9ca7882ac9a2227c5d0284c88655b261a2fe85db270",
     });
     const result = await provider.request(request);
